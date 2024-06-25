d81bf1b13197b473798730fa59774b67a113d8e3
Calum Dingwall <29152895+caburum@users.noreply.github.com>
2024-06-25 00:09:53 -0400

    print total hours & show in local timezone

--- plotHours.py
+++ plotHours.py
@@ -1,5 +1,5 @@
 import api
-from datetime import datetime
+from datetime import datetime, UTC
 import plotly.express as px
 import pandas as pd
 
@@ -11,13 +11,14 @@ print(activity)
 
 gamertags = api.batchGetGamertags(list(activity.keys()))
 
-# totalHours = pd.DataFrame([
-# 	dict(gamertag=gamertags[xuid], hours=sum([(d['e'] - d['s']) / 3600 for d in activity[xuid]]))
-# 	for xuid in activity
-# ])
+userTotalHours = pd.DataFrame([
+	dict(gamertag=gamertags[xuid], hours=sum([(d['e'] - d['s']) / 3600 for d in activity[xuid]]))
+	for xuid in activity
+])
+print(userTotalHours)
 
 df = pd.DataFrame([
-	dict(gamertag=gamertags[xuid], start=datetime.utcfromtimestamp(d['s']), end=datetime.utcfromtimestamp(d['e']), hours=((d['e'] - d['s']) / 3600))
+	dict(gamertag=gamertags[xuid], start=datetime.fromtimestamp(d['s'], UTC).astimezone(), end=datetime.fromtimestamp(d['e'], UTC).astimezone(), hours=((d['e'] - d['s']) / 3600))
 	for xuid in activity for d in activity[xuid]
 ])
 fig = px.timeline(df, x_start='start', x_end='end', y='gamertag', hover_data="hours", template='plotly_dark')
